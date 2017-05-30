8aa53d16b7e504900c9976593636849459d59c69
martincz <martincz@mokeedev.com>
2017-05-30 15:31:27 +0800

    hlte: mokee-ify

---
+++ mokee.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/samsung/hlte/full_hlte.mk)
+
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+PRODUCT_DEVICE := hlte
+PRODUCT_NAME := mk_hlte
